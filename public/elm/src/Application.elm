0a0d3c3ffc2048b468afc1e0c53781d58c841f25
ronen <narkisr@gmail.com>
2015-11-26 20:07:44 +0200

    adding elm source

---
+++ public/elm/src/Application.elm
@@ -0,0 +1,80 @@
+module Application where
+
+import Html exposing (..)
+import Html.Attributes exposing (type', class, id, style)
+import Effects exposing (Effects, Never, batch, map)
+import Set
+import Http exposing (Error(BadResponse))
+import Task
+import Json.Decode as Json exposing (..)
+
+import Debug
+
+init : (Model, Effects Action)
+init =
+    (Model [], getRepos)
+
+type alias Repository = 
+ {url : String, name : String} 
+
+type alias Model = 
+  { repos : List Repository }
+
+type Action = 
+   Load (Result Http.Error (List Repository))
+
+update : Action ->  Model-> (Model , Effects Action)
+update action model =
+  case action of 
+   Load result -> 
+     case result of
+        Result.Ok repos -> 
+          ({ model | repos <- repos }, Effects.none)
+        Result.Err e -> 
+          case e of 
+             _ -> 
+               Debug.log (toString e) (model , Effects.none)
+
+partition n list =
+  let
+    catch = (List.take n list)
+  in 
+    if n == (List.length catch) then
+      [catch] ++ (partition n (List.drop n list))
+    else
+      [catch]
+
+repoPanel : Repository -> Html
+repoPanel repo =
+  div [class "col-md-4 col-xs-4"] [
+    div [class "panel panel-default "]  [
+       div [class "panel-heading", style [("background", "red")]] [(text repo.name)]
+     , div [class "panel-body"] [(text ("url: " ++ repo.url))]
+    ]
+  ]
+
+view : Signal.Address Action -> Model -> Html
+view address ({repos} as model) = 
+  div  [class "container-fluid"] 
+    (List.map (\pair ->  div [class "row"] pair) (partition 3 (List.map repoPanel repos)))
+  
+
+repo : Decoder Repository
+repo = 
+  object2 Repository
+    ("url" := string)
+    ("name" := string)
+
+ 
+repos : Decoder (List Repository)
+repos = 
+ at ["repos"] (list repo)
+  
+
+getRepos: Effects Action
+getRepos = 
+  Http.get repos ("/repos/list")
+    |> Task.toResult
+    |> Task.map Load
+    |> Effects.task
+
